18f37607245341f79c135912abcecc561f7cf06d
Barış Bükümcüler <barisbukumculer@gmail.com>
2023-08-11 23:39:26 +0300

    bootsrap and jquery angular
    frontend cont.

--- src/app/admin/layout/components/components.module.ts
+++ src/app/admin/layout/components/components.module.ts
@@ -3,6 +3,9 @@ import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
+import { RouterModule } from '@angular/router';
+import {MatListModule} from '@angular/material/list';
+
 
 
 
@@ -13,7 +16,9 @@ import { SidebarComponent } from './sidebar/sidebar.component';
     SidebarComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule,
+    MatListModule
   ],
   exports:[
     HeaderComponent,
